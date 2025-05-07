4f5a7e48a3766591af2251718ad5c8373e83a476
23ji <isj4194@naver.com>
2025-05-07 20:18:33 +0900

    Chat 화면에서 back 버튼 숨기기

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -10,17 +10,17 @@ import FirebaseAuth
 import UIKit
 
 class ChatViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var messageTextfield: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-    @IBAction func sendPressed(_ sender: UIButton) {
-    }
-    
-
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var messageTextfield: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationItem.hidesBackButton = true
+  }
+  
+  @IBAction func sendPressed(_ sender: UIButton) {
+  }
+  
+  
 }
