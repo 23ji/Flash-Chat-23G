cadd6e036d435036beb05d5d17e015f5f7b2aaad
23ji <isj4194@naver.com>
2025-05-08 17:43:52 +0900

    로그아웃 기능 복습

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -36,43 +36,22 @@ class ChatViewController: UIViewController {
   
   // 복습하기 ⭐️
   @IBAction func logout(_ sender: UIBarButtonItem) {
-    do { 
-      try Auth.auth().signOut()
+    do { try Auth.auth().signOut()
       self.navigationController?.popToRootViewController(animated: true)
-    } catch let error as NSError {
+    }
+    catch let error as NSError {
       print(error)
     }
   }
   
   // 복습하기 ⭐️
   @IBAction func sendPressed(_ sender: UIButton) {
-    guard let sender = Auth.auth().currentUser?.email else { return }
-    guard let body = self.messageTextfield.text else { return }
-    guard body.isEmpty == false else { return }
     
-    db.collection(K.FStore.collectionName).addDocument(data: [
-      K.FStore.senderField: sender,
-      K.FStore.bodyField: body
-    ])
   }
   
   // 복습하기 ⭐️
   func loadMessages() {
-    db.collection(K.FStore.collectionName).getDocuments { querySnapShot, error in
-      guard let snapShotData = querySnapShot?.documents else { return }
-      for document in snapShotData {
-        let doc = document.data()
-        
-        if let sender = doc["sender"] as? String ,
-           let body = doc["body"] as? String {
-          let message = Message(sender: sender, body: body)
-          self.messages.append(message)
-        }
-      }
-      DispatchQueue.main.async {
-        self.tableView.reloadData()
-      }
-    }
+    
   }
   
   
@@ -86,7 +65,7 @@ extension ChatViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = self.tableView.dequeueReusableCell(
       withIdentifier: K.cellIdentifier,
-      for: indexPath) as? MessageCell else { return UITableViewCell()}
+      for: indexPath) as? MessageCell else { return UITableViewCell() }
     cell.label.text = self.messages[indexPath.row].body
     return cell
   }
