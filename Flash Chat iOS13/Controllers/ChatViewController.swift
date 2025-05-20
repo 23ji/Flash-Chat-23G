6d4ca54e067b752a4a5634d7cfaf48f56ed38f10
23ji <isj4194@naver.com>
2025-05-20 21:02:07 +0900

    navi bar Back Btn 숨기기

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -10,17 +10,15 @@ import FirebaseAuth
 import UIKit
 
 class ChatViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var messageTextfield: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-    @IBAction func sendPressed(_ sender: UIButton) {
-    }
-    
-
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var messageTextfield: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationItem.setHidesBackButton(true, animated: true)
+  }
+  
+  @IBAction func sendPressed(_ sender: UIButton) {
+  }
 }
