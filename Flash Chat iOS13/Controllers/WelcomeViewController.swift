bdfdc0d35b0fa95643d5a63da3efcc4e9fd31089
23ji <isj4194@naver.com>
2025-05-20 20:20:26 +0900

    3차 복습 시작

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -5,14 +5,13 @@
 //  Created by Angela Yu on 21/10/2019.
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
-import  CLTypingLabel
+//import  CLTypingLabel
 
 import UIKit
 
 class WelcomeViewController: UIViewController {
   
-  
-  @IBOutlet weak var titleLabel: CLTypingLabel!
+  @IBOutlet weak var titleLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
