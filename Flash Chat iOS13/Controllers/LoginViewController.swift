49bd60185a3ab999eee3b69cb2d4c040367bb89a
23ji <isj4194@naver.com>
2025-05-20 20:51:32 +0900

    로그인 기능 구현

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Angela Yu on 21/10/2019.
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
-
+import FirebaseAuth
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -15,6 +15,12 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+      guard let email = emailTextfield.text else { return }
+      guard let password = passwordTextfield.text else { return }
+      
+      Auth.auth().signIn(withEmail: email, password: password) { result, error in
+        guard error == nil else { return }
+        self.performSegue(withIdentifier: "LoginToChat", sender: nil)
+      }
     }
-    
 }
