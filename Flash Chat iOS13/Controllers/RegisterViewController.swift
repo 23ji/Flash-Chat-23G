c960638fc1af9f6a1f2cb20a14d872f9f63f7653
23ji <isj4194@naver.com>
2025-05-20 20:51:07 +0900

    회원가입 기능 구현

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Angela Yu on 21/10/2019.
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
-
+import FirebaseAuth
 import UIKit
 
 class RegisterViewController: UIViewController {
@@ -14,6 +14,14 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+      
+      guard let email = emailTextfield.text else { return }
+      guard let password = passwordTextfield.text else { return }
+      
+      Auth.auth().signIn(withEmail: email, password: password) { result, error in
+        guard error == nil else { return }
+        self.performSegue(withIdentifier: "RegisterToChat", sender: nil)
+      }
     }
     
 }
